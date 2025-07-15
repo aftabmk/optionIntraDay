89830fe9b7ad261a4c828dd5bc298e2a67792b83
Aftab mohammed k <87110394+aftabmk@users.noreply.github.com>
2025-07-15 19:31:24 +0530

    modified:   getData/utils.js

--- getData/utils.js
+++ getData/utils.js
@@ -3,4 +3,17 @@ function splitInChunks(input) {
   return [str.slice(0, 2), str.slice(2, 4), str.slice(4, 6)];
 }
 
-module.exports = { splitInChunks };
+function formatTime(isoString) {
+  const date = new Date(isoString);
+
+  // Use UTC time directly
+  let hours = date.getUTCHours();
+  const minutes = date.getUTCMinutes();
+  const ampm = hours >= 12 ? 'pm' : 'am';
+
+  hours = hours % 12 || 12; // Convert 0 to 12
+
+  return `${hours}:${minutes.toString().padStart(2, '0')}${ampm}`;
+}
+
+module.exports = { splitInChunks, formatTime};
